7a1089d869659e04c31f991784d8c9456cd6a091
smanish255 <103683199+smanish255@users.noreply.github.com>
2022-12-23 16:02:28 +0530

    Create assignment_operator.py
    multiline assignment

---
+++ assignment_operator.py
@@ -0,0 +1,5 @@
+a=float(input("enter a value = "))
+b=float(input("enter b value = "))
+add,sub,mul,div,fdiv,mdiv,expo=a+b,a-b,a*b,a/b,a//b,a%b,a**b
+print("\t addition of a,b:{},substraction of a,b:{},multiplication of a,b:{},division of a,b:{},floor division of a,b:{},modular division of a,b:{},exponentital of a,b:{}".format (a+b,a-b,a*b,a/b,a//b,a%b,a**b))
+      
